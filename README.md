5d8fe54cd53f4af449560bda56755dbcf266e39e
guadalupe <guadalupe.alavila@gmail.com>
2023-09-02 14:13:29 -0300

    create new pages

--- README.md
+++ README.md
@@ -3,10 +3,32 @@
 Curso Legacy: ionic 6: Crear aplicaciones IOS, Android con Angular.
 https://www.udemy.com/course/ionic-ios-android-pwa-appstore-playstore-push/
 
-Levantar proyecto
+## 🚀 Comandos
 
 ```bash
   ionic serve
 
   ionic serve --lab
+```
+
+### Crear Page
+
+Para ver que va a crear el CLI de ionic con el comando
+
+```bash
+  ionic g page pages/home --dry-run
+```
+
+Crear página
+
+```bash
+  ionic g page pages/home
+
+```
+
+Crear página sin archivo de prueba
+
+```bash
+  ionic g page pages/home --spec=false
+
 ```
--- components-app/src/app/app-routing.module.ts
+++ components-app/src/app/app-routing.module.ts
@@ -1,22 +1,43 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+// en app-routing.module definimos las rutas principales de la aplicación.
+
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: 'alerts',
+    loadChildren: () =>
+      import('./pages/alerts/alerts.module').then((m) => m.AlertsPageModule),
+  },
+
+  {
+    path: 'action-sheet',
+    loadChildren: () =>
+      import('./pages/action-sheet/action-sheet.module').then(
+        (m) => m.ActionSheetPageModule
+      ),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
